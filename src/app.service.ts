d6e94d3f64dd914fadee2a3a6f719d8b4b80a741
Edward Eleazar <eleazaredward@gmail.com>
2026-01-29 12:50:36 +0100

    chore(main): added health endpoint and some packages

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World Gang!';
   }
+
+  checkHealth(): string {
+    return `Health check passed! (1/1) - ${new Date().toISOString()}`;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Security header middleware
+  app.use(helmet());
+
+  // Cors
+  app.enableCors({
+    origin: ["*"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  })
+
+  // Global prefix
+  app.setGlobalPrefix('api/v1');
+
+  const PORT = process.env.PORT ?? 3000
+  await app.listen(PORT);
+
+  console.log(`Server is running on: http://localhost:${PORT}/api/v1`);
+  // if (configService.get('NODE_ENV') !== 'production') {
+  //   console.log(`Swagger documentation: http://localhost:${port}/api-docs`);
+  // }
 }
+
 bootstrap();
