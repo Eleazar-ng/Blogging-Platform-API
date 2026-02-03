3ed7f94a3b561bd8a5722755d0bc808a3d3025a7
Edward Eleazar <eleazaredward@gmail.com>
2026-02-03 19:33:43 +0100

    chore(auth): added auth guard to protect routes

---
+++ src/auth/guards/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from '../../user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private userService: UserService,
+    private configService: ConfigService
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKeyProvider: (request, rawJwtToken, done) => {
+        const secret = this.configService.get<string>('JWT_SECRET')
+        done(null, secret);
+      },
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.userService.findById(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
